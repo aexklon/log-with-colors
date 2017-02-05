bc00efc33a284d5d7d83c333663ccdaa01bec32a
alexandre lopes <alexfl.dsg@gmail.com>
2017-02-05 20:44:49 -0200

    sucess, warn and info converted to pointers

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # log-with-colors
-simple syntax for logging to console using colors, based on chalk.js
+Simple syntax for logging to console using colors, based on [chalk](https://github.com/chalk/chalk)
+```javascript
+log.green('This will log to the console in the green color')
+```
+
+## Install
+
+```console
+$ npm i -S log-with-colors
+```
+
+## Usage
+
+```javascript
+const log = require('log-with-colors')
+```
+```javascript
+log.sucess('Hello World') //logs to the console in the green color
+```
+
+## API
+
+| Function      | Type          | Info          |
+| ------------- |:-------------:|:-------------:|
+| log()         | default       | Prints with process.stdout |
+| log.error()   | error         | Prints with process.stderr |
+| log.success() | color         | Equals to log.green()   |
+| log.warn()    | color         | Equals to log.yellow()  |
+| log.info()    | color         | Equals to log.cyan()    |
+
+| Function      | Type          |
+| ------------- |:-------------:|
+| log.gray()    | color         |
+| log.blue()    | color         |
+| log.cyan()    | color         |
+| log.green()   | color         |
+| log.yellow()  | color         |
+| log.red()     | color         |
+| log.magenta() | color         |
+
+## License
+MIT © [Alexandre Lopes](https://alexandrelopes.design)
--- index.js
+++ index.js
@@ -2,29 +2,30 @@ const chalk = require('chalk')
 const util = require('util')
 const print = (logLevel, message) => {
 	switch(logLevel.type) {
-		case 'log': {
-    	process.stdout.write(message + '\n');
-			break
-		}
 		case 'error': {
-    	process.stderr.write(chalk[logLevel.color](message) + '\n');
+    	process.stderr.write(chalk[logLevel.color](message) + '\n')
 			break
 		}
 		case 'color': {
-    	process.stdout.write(chalk[logLevel.color](message) + '\n');
+    	process.stdout.write(chalk[logLevel.color](message) + '\n')
+			break
+		}
+		default: {
+    	process.stdout.write(message + '\n')
 			break
 		}
 	}
 	return undefined
 }
-const log =		(...args) => print({type:'log'}, util.format(...args))
-log.red =			(...args) => print({type:'color', color:'red'}, util.format(...args))
-log.cyan =		(...args) => print({type:'color', color:'cyan'}, util.format(...args))
-log.yellow =	(...args) => print({type:'color', color:'yellow'}, util.format(...args))
-log.green =		(...args) => print({type:'color', color:'green'}, util.format(...args))
-log.magenta =	(...args) => print({type:'color', color:'magenta'}, util.format(...args))
-log.blue =		(...args) => print({type:'color', color:'blue'}, util.format(...args))
+const log =		(...args) => print({type:'default'}, util.format(...args))
 log.gray =		(...args) => print({type:'color', color:'gray'}, util.format(...args))
+log.blue =		(...args) => print({type:'color', color:'blue'}, util.format(...args))
+log.cyan =		(...args) => print({type:'color', color:'cyan'}, util.format(...args))
+log.green =		(...args) => print({type:'color', color:'green'}, util.format(...args))
+log.yellow =	(...args) => print({type:'color', color:'yellow'}, util.format(...args))
+log.red =			(...args) => print({type:'color', color:'red'}, util.format(...args))
+log.magenta =	(...args) => print({type:'color', color:'magenta'}, util.format(...args))
+
 
 log.error =		(...args) => print({type:'error', color:'red'}, util.format(...args))
 log.info =		log.cyan
